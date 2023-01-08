b93a69e968f06f318c78235780dba0c9ee1efca8
newtronahmed <bazubairu@st.ug.edu.gh>
2023-01-08 16:12:57 +0000

    text in mobile size restored on hero section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -16,15 +16,16 @@ function Index() {
             <div className="container lg:grid lg:grid-cols-3 mt-[4rem] sm:mt-0 px-8 sm:px-0 gap-4 ">
                 <div className="col-span-2 w-full relative bg-blue overflow-hidden h-[calc(100vh_-_12rem)] sm:h-auto  sm:pr-0 px-8 rounded-[2rem]">
                     <div className="grid grid-cols-5 h-full">
-                        <div className="left col-span-2 py-6 flex flex-col sm:justify-between">
+                        <div className="left col-span-3 sm:col-span-2 py-6 flex flex-col sm:justify-between">
                             <div className='basis-2/3'>Curated playlist</div>
                             <div className='basis-1/3'>
                                 <h3 className='text-lg lg:text-4xl font-bold'>R & B Hits</h3>
                                 <div className='text-sm lg:text-md'>
-                                    All mine, Lie again, Petty call me everyday, Out of time, No love, Bad habit, and so much moreF
+                                <span className='block sm:inline'>All mine, Lie again, Petty call me everyday,</span>
+                                <span className='block sm:inline'>Out of time, No love, Bad habit, and so much more</span> 
                                 </div>
                             </div>
-                            <div className='flex items-center '>
+                            <div className='flex items-center mt-2 sm:mt-0'>
                                 <div className='ml-4 flex justify-between'>
                                     <img className=' -ml-[1rem]' src="/asset/avatar_sm.png" alt="avatar" />
                                     <img className=' -ml-[1rem]' src="/asset/avatar_sm.png" alt="avatar" />
@@ -36,11 +37,11 @@ function Index() {
                                     <img src="/asset/like.svg" alt="like" />
                                 </div>
                                 <div>
-                                    <span className='lg:text-lg'>33k likes</span>
+                                    <span className='text-xs sm:text-lg flex'>33k likes</span>
                                 </div>
                             </div>
                         </div>
-                        <div className="right relative col-span-3  " >
+                        <div className="right relative sm:col-span-3 col-span-2  " >
                             <img src="/asset/wave.svg" className=' absolute -top-[3rem] -right-[4rem] sm:top-0 sm:right-0 sm:h-full sm:w-full rotate-90 sm:rotate-0' alt="wave" />
                             <img className=' z-[1] bottom-0 hidden sm:block sm:absolute right-[-1rem]' src={banner_1} alt="banner-1" />
                         </div>
